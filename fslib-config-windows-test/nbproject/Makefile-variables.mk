e4ab21aba18e5388da2212fecbb047b0f1bbce4e
zhangrui <zhangrui@fslib.org>
2022-11-04 11:45:13 +0800

    初始化项目

---
+++ fslib-config-windows-test/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# mingw64-Release configuration
+CND_PLATFORM_mingw64-Release=MinGW64-Windows
+CND_ARTIFACT_DIR_mingw64-Release=.
+CND_ARTIFACT_NAME_mingw64-Release=libfslib-config-test.exe
+CND_ARTIFACT_PATH_mingw64-Release=./libfslib-config-test.exe
+CND_PACKAGE_DIR_mingw64-Release=dist/mingw64-Release/MinGW64-Windows/package
+CND_PACKAGE_NAME_mingw64-Release=fslib-config-windows-test.tar
+CND_PACKAGE_PATH_mingw64-Release=dist/mingw64-Release/MinGW64-Windows/package/fslib-config-windows-test.tar
+# mingw64-Debug configuration
+CND_PLATFORM_mingw64-Debug=MinGW64-Windows
+CND_ARTIFACT_DIR_mingw64-Debug=.
+CND_ARTIFACT_NAME_mingw64-Debug=libfslib-config-test.exe
+CND_ARTIFACT_PATH_mingw64-Debug=./libfslib-config-test.exe
+CND_PACKAGE_DIR_mingw64-Debug=dist/mingw64-Debug/MinGW64-Windows/package
+CND_PACKAGE_NAME_mingw64-Debug=fslib-config-windows-test.tar
+CND_PACKAGE_PATH_mingw64-Debug=dist/mingw64-Debug/MinGW64-Windows/package/fslib-config-windows-test.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ fslib-config-windows-test/nbproject/private/Makefile-variables.mk
@@ -0,0 +1,7 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+# mingw64-Release configuration
+# mingw64-Debug configuration
